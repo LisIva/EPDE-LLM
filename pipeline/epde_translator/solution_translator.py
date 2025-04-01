Fix small bugs in solution_translator and start rolling back llm_pool.terms_max_num calculation

pipeline/epde_translator/solution_translator.py
@@ -43,11 +43,11 @@ class BaseTokenConverter(object):
                 return 'du/dx0'
             elif self.token == 'du_dx':
                 return 'du/dx1'
-            elif len(self.token) == 7:
-                return self.__convert_high_deriv(pool)
+            elif len(self.token) == 7 or len(self.token) == 9:
+                return self.__convert_high_deriv(pool, len(self.token))
 
-    def __convert_high_deriv(self, pool: LLMPool):
-        var = self.token[-2]
+    def __convert_high_deriv(self, pool: LLMPool, token_len: int):
+        var = self.token[-2] if token_len == 7 else self.token[-3]
         n = int(self.token[-1])
 
         pool.terms_max_num += n-1.0
@@ -114,7 +114,10 @@ class MulConverter(object):
         self.llm_pool = llm_pool
 
     def to_epde(self):
+        if self.mul_term.is_Float:
+            return str(self.mul_term)
         mul_str = [str(self.mul_term.args[0]), ]
+
         if self.llm_pool.factors_max_num < len(self.mul_term.args)-1:
             self.llm_pool.factors_max_num = len(self.mul_term.args)-1
 
@@ -153,9 +156,12 @@ class SolutionTranslator(object):
 
     def translate(self):
         cached_pool = self.llm_pool.deepcopy()
-        cached_pool.terms_max_num = 0.0
+        # cached_pool.terms_max_num = 0.0
         left_side = ' = ' + BaseTokenConverter(self.left_deriv, 1).convert(cached_pool) + '{power: 1.0}'
         add_str = []
+
+        if cached_pool.terms_max_num < len(self.sympy_code.args):
+            cached_pool.terms_max_num = len(self.sympy_code.args)
 
         if self.sympy_code.is_Add:
             for mul_term in self.sympy_code.args:
@@ -187,12 +193,12 @@ if __name__ == '__main__':
                  'du/dt = c[0] * du/dx + c[1] * du/dt * d^2u/dx^2': (1.75, 542.9853705131861),
                  'du/dt = c[0] * du/dx + c[1] * t * du/dx': (1.2, 442.49077370655203)}
     rs1 = 'params[0] * derivs_dict["du/dx"] ** 3 * t + ((params[1] * derivs_dict["du/dx"] ** 2 * np.cos(params[2] * t +1)) * x**2) * u +params[3] * derivs_dict["du/dt"] * (t**2 + 2)'
-    rs2 = 'params[0] * derivs_dict["d^2u/dt^2"] ** 2 * t**3 + params[1] * derivs_dict["du/dx"] * np.arcsin(params[2] * t**2) * x**3'
+    rs2 = 'params[0] * derivs_dict["d^2u/dt^2"] ** 2 * t**3 + params[1] * derivs_dict["du/dx"] * np.arcsin(params[2] * t**2 + u) * x**3'
     rs3 = 'params[0] * np.arcsin(1.67335*t**2*x)'
     llm_pool = LLMPool()
-    st3 = SolutionTranslator(rs3, np.array([2.1, ]), llm_pool, 'sindy-burg').translate()
-    st1 = SolutionTranslator(rs1, np.array([1.2, 5.678, 6.5421, 4.12]), llm_pool, 'sindy-burg').translate()
-    st2 = SolutionTranslator(rs2, np.array([2.1, 10.1, 9.2]), llm_pool, 'sindy-burg').translate()
+    st3 = SolutionTranslator(rs3, np.array([2.1, ]), llm_pool, 'burg_sindy').translate()
+    st1 = SolutionTranslator(rs1, np.array([1.2, 5.678, 6.5421, 4.12]), llm_pool, 'burg_sindy').translate()
+    st2 = SolutionTranslator(rs2, np.array([2.1, 10.1, 9.2]), llm_pool, 'burg_sindy').translate()
 
     epde_classes, lambda_strs = llm_pool.to_epde_classes()
 