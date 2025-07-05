c1a4560949e33a9f34d00307cc073d07b801ec33
LisIva <lizavvveta99@gmail.com>
2025-07-05 15:56:41 +0300

    Prepare pipeline_main for several runs (max_llm_run)

--- pipeline/pipeline_main.py
+++ pipeline/pipeline_main.py
@@ -1,30 +1,38 @@
 from pipeline.optimization_workflow.optimization_manager import OptManager
 from epde_struct_evaluator.epde_struct_evaluator import TrackEvaluator
+from epde_eq_parse.eq_evaluator import EqReranker
 import time
 
-max_iter = 3
+max_llm_run = 1
+max_iter = 1
 dir_name = 'burg_sindy'
 start_iter = 0
 refine_point = 100
 
 debug = True # True False
 print_exc = True
-exit_code = True
+exit_code = False
 
 
 if __name__ == '__main__':
-    iter_num = 0
+    best_info_ls = []
 
-    t1 = time.time()
-    opt_manager = OptManager(max_iter, start_iter, refine_point, dir_name, debug, print_exc, exit_code,
-                             resample_shape=(20, 20), n_candidates=4)
-    opt_manager.explore_solutions()
-    pruned_track, by_project_track = opt_manager.call_pruner()
+    for iter_num in range(max_llm_run):
+        t1 = time.time()
+        opt_manager = OptManager(max_iter, start_iter, refine_point, dir_name, debug, print_exc, exit_code,
+                                 resample_shape=(20, 20), n_candidates=4)
+        opt_manager.explore_solutions()
+        pruned_track, by_project_track = opt_manager.call_pruner()
 
-    t2 = time.time()
+        t2 = time.time()
 
-    te = TrackEvaluator(dir_name, opt_manager.eq_buffer.full_records_track, pruned_track, t2-t1, iter_num)
-    te.evaluate()
+        te = TrackEvaluator(dir_name, opt_manager.eq_buffer.full_records_track, pruned_track, t2-t1, iter_num)
+        best_eq_info = te.evaluate()
+        best_info_ls.append(best_eq_info)
+
+    eq_r = EqReranker(best_info_ls, dir_name)
+    eq_r.best_run_inf = best_info_ls
+    # eq_r.to_csv()
     print()
 
 
