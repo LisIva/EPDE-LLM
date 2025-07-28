3eec2bf102acaab95f0350ee03cfc81fe2719840
LisIva <lizavvveta99@gmail.com>
2025-07-28 23:29:49 +0300

    Clean output dir for all llm_runs (not just 1)

--- pipeline/clean_directories.py
+++ pipeline/clean_directories.py
@@ -2,14 +2,15 @@ import os
 import sys
 from pathlib import Path
 import shutil
-from pipeline.rebuild_prompt import extract_exp_buffer
+# from pipeline.rebuild_prompt import extract_exp_buffer
+import re
 
 
 PARENT_PATH = Path(os.path.dirname(__file__)).parent
 
 
-def clean_output_dir(dir_path="debug_llm_outputs"):
-    dir_path = os.path.join(PARENT_PATH, "pipeline", dir_path)
+def clean_output_dir(llm_iter=0, dir_path="debug_llm_outputs"):
+    dir_path = os.path.join(PARENT_PATH, "pipeline", dir_path, f"llm_iter_{llm_iter}")
     if len(os.listdir(dir_path)) == 0:
         print(f"The directory {dir_path} is already empty")
 
@@ -21,6 +22,20 @@ def clean_output_dir(dir_path="debug_llm_outputs"):
             print(f"The directory {dir_path} doesn't exist")
 
 
+def delete_out_folders(dir_paths=None):
+    if dir_paths is None:
+        dir_paths = ["prompts", "debug_llm_outputs"]
+
+    for dir_name in dir_paths:
+        parent_dir = os.path.join(PARENT_PATH, "pipeline", dir_name)
+        for folder_name in os.listdir(parent_dir):
+            folder_path = os.path.join(parent_dir, folder_name)
+            if os.path.isdir(folder_path) and re.match(r"^llm_iter_\d+$", folder_name):
+                try:
+                    shutil.rmtree(folder_path)
+                except Exception as e:
+                    print(f"Error deleting {folder_path}: {e}")
+
 # def reset_buffer_prompt(path):
 #     переписать path используя PARENT_PATH
 #     start_pos, end_pos, dict_str, file_content = extract_exp_buffer(path)
@@ -29,16 +44,26 @@ def clean_output_dir(dir_path="debug_llm_outputs"):
 #         prompt.write(new_content)
 
 
-def reset_prompt_to_init(dir_path="prompts"):
+def create_llm_iter_n_folders(llm_iter=0, dir_paths=None):
+    if dir_paths is None:
+        dir_paths = ["prompts", "debug_llm_outputs"]
+
+    for path in dir_paths:
+        path = os.path.join(PARENT_PATH, "pipeline", path, f"llm_iter_{llm_iter}")
+        os.makedirs(path, exist_ok=True)
+
+
+def reset_prompt_to_init(llm_iter=0, dir_path="prompts"):
     source = os.path.join(PARENT_PATH, "pipeline", dir_path, "reset-for-continue.txt")
-    target = os.path.join(PARENT_PATH, "pipeline", dir_path, "continue-iter.txt")
+    target = os.path.join(PARENT_PATH, "pipeline", dir_path, f"llm_iter_{llm_iter}", "continue-iter.txt")
     shutil.copyfile(source, target)
 
     refine_source = os.path.join(PARENT_PATH, "pipeline", dir_path, "reset-for-continue-refinement.txt")
-    refine_target = os.path.join(PARENT_PATH, "pipeline", dir_path, "continue-iter-refinement.txt")
+    refine_target = os.path.join(PARENT_PATH, "pipeline", dir_path, f"llm_iter_{llm_iter}", "continue-iter-refinement.txt")
     shutil.copyfile(refine_source, refine_target)
 
 
 if __name__ == "__main__":
     # clean_output_dir()
-    reset_prompt_to_init()
+    # reset_prompt_to_init()
+    delete_out_folders()
